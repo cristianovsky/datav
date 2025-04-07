bed754f03366e923d3ee92330c706829ddb9cc4d
cristianovsky <cristianovsky@gmail.com>
2025-04-07 11:21:27 -0500

    actualizacion 6 era dejar app.run(pero con parameters puerto)

--- main.py
+++ main.py
@@ -223,4 +223,4 @@ app.layout = html.Div(style=styles['container'], children=[
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
